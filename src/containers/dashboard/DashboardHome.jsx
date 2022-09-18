d2939c640ebc46713581b06f14003db5f8ba0b29
Firdaws Lamidi <lamidifirdaws@gmail.com>
2022-09-18 21:46:29 +0100

    dashboard home rwd

--- src/containers/dashboard/DashboardHome.jsx
+++ src/containers/dashboard/DashboardHome.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 const DashboardHome = () => {
   return (
     <>
- <div className="mt-[90px] pl-[10px] py-[20px] ">
+    <div className="lg:absolute lg:left-[280px]">
+ <div className="mt-[90px] lg:mt-[10px] pl-[10px] py-[20px] ">
       <h1 className="text-[17px] md:text-[22px] lg:text-[28px] font-[500]">
         Welcome back,!
       </h1>
     </div>
-    <div className="flex flex-col items-center">
-    <div className="w-[300px] h-[200px]  my-[10px] bg-green rounded-[5px] xl:w-[700px] mx-[5px] py-[20px] shadow-md ">
+    <div className="flex flex-col items-center lg:items-start">
+    <div className="w-[300px] h-[200px]  my-[10px] bg-green rounded-[5px] md:w-[400px] md:h-[250px] xl:h-[300px] xl:w-[500px] md:py-[35px] mx-[5px] py-[20px] shadow-md ">
       <div className="flex flex-col items-center">
       <div className="flex flex-row justify-center items-center rounded-[50%] border-[2px] border-white w-[50px] h-[50px]">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="white" className="w-6 h-6 ">
@@ -17,9 +18,10 @@ const DashboardHome = () => {
 </svg>
 </div> 
  </div>
-<h2 className="text-[35px] md:text-[30px] xl:text-[50px] font-[600] text-center py-[5px] text-white">Status</h2>
-<p className="text-[22px] md:text-[30px] xl:text-[50px] font-[600] text-center py-[5px] text-white">All Systems Operational</p>
+<h2 className="text-[35px] md:text-[30px] xl:text-[40px] font-[600] text-center py-[5px] text-white">Status</h2>
+<p className="text-[22px] md:text-[25px] xl:text-[35px] font-[500] text-center py-[5px] text-white">All Systems Operational</p>
 
+</div>
 </div>
 </div>
     </>
--- src/containers/dashboard/DashboardNav.jsx
+++ src/containers/dashboard/DashboardNav.jsx
@@ -10,6 +10,7 @@ const DashboardNav = () => {
 
   return (
     <>
+    <div className="lg:hidden">
 <div className={ !nav ? 'fixed top-0 bg-blue h-[60px] w-[100%]' : 'fixed top-0 left-0 min-h-[100vh] w-[250px]'}>
     <nav className={ !nav ? 'hidden' : 'flex border-r  min-h-[100vh] w-[250px] pt-8 px-1  flex-col items-start justify-between bg-blue text-white'}>
         <div className="flex flex-col">
@@ -40,7 +41,33 @@ const DashboardNav = () => {
              <div className="lg:hidden" onClick={handleClick}>
     {!nav ? <MenuIcon className='w-10 text-white m-2 absolute top-[2px] right-0' /> : <XIcon className='w-9 text-white m-2 absolute top-[24px] left-[199px]'/>}
 </div>
-
+</div>
+{/* Desktop Nav */}
+<div className="hidden lg:block">
+<div className='fixed top-0 left-0 min-h-[100vh] w-[250px]'>
+    <nav className='flex border-r  min-h-[100vh] w-[250px] pt-8 px-1  flex-col items-start justify-between bg-blue text-white'>
+        <div className="flex flex-col">
+            <div className="flex flex-col ">
+      <div className="pl-[20px] mt-[-30px]"><img src={logo} alt="Logo" className='w-[100px] h-[100px] ' /></div>
+    </div>
+     <div className='space-y-8 w-full'>
+   {navLinks.map((link) => 
+          <NavItem link={link} key={link.id} />
+        )}
+        <div className="flex items-center space-x-8 px-5  cursor-pointer">
+            <span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" /></svg></span>
+      <button className="text-light-black group-hover:text-black group-hover:font-[900]">
+      Log out
+      </button>
+    </div>
+      </div>
+     
+</div>
+    </nav>
+    
+    </div>
+    </div>
    </>
     )
 function NavItem({ link }) {
